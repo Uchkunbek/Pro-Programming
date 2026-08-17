d8f3528446ce074f4887f0f0f55f8a596b3c6943
uchkunbek <Temur2014@@>
2026-08-17 15:53:52 -0400

    Sonni topish o'yini

---
+++ darslar/Sonni topish o'yini.py
@@ -0,0 +1,15 @@
+#Sonni topish o'yini
+
+# import random
+#
+# son = random.randint(1, 100)
+# print(son)
+# while True:
+#         taxmin = int(input("Taxmin: "))
+#         if taxmin < son:
+#             print("Juda kichik")
+#         elif taxmin > son:
+#             print("Juda katta")
+#         else:
+#             print("Yutdingiz")
+#             break
